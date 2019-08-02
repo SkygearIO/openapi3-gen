835511b0e48860f0d2f18c338e41c70012b195a6
kiootic <kiootic@oursky.com>
2019-08-02 17:45:27 +0800

    Add argument parsing

--- cmd/openapi3-gen/main.go
+++ cmd/openapi3-gen/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/skygeario/openapi3-gen/internal"
+)
+
+var outputFile string
+
+func init() {
+	flag.StringVar(&outputFile, "output", "", "output OpenAPI specification file")
+	flag.StringVar(&outputFile, "o", "", "output OpenAPI specification file (shorthand)")
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: %s [flags] <patterns...>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+	patterns := flag.Args()
+
+	if len(patterns) == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	workDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	err = internal.Run(workDir, patterns, outputFile)
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ internal/main.go
@@ -0,0 +1,5 @@
+package internal
+
+func Run(workDir string, patterns []string, outputFile string) error {
+	return nil
+}
